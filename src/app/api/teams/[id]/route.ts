2d0c0c8908ee4fecaa56e5fe46395c36d092d7f3
Raven <nathaliorakotobe3@gmail.com>
2026-03-07 14:27:06 +0300

    fix nextjs 16 params promise

--- src/app/api/teams/[id]/route.ts
+++ src/app/api/teams/[id]/route.ts
@@ -1,12 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } }
+  
 ) {
   try {
-
-    const id = context.params.id;
 
     const count = 0;
 
