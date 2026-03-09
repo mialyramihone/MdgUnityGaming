b93cc0cbcab3585641d87840ec82a1b187ee7ec2
mialyramihone <ramihonemialy@gmail.com>
2026-03-09 08:01:52 +0300

    maj2

--- netlify/functions/team-registration.js
+++ netlify/functions/team-registration.js
@@ -1,5 +1,5 @@
 const { neon } = require('@neondatabase/serverless');
-const multiparty = require('multiparty');
+const multipartParser = require('lambda-multipart-parser');
 
 exports.handler = async (event) => {
   console.log('🔵 Fonction team-registration appelée');
@@ -23,11 +23,13 @@ exports.handler = async (event) => {
   }
 
   try {
-    const formData = await parseFormData(event);
-    const fields = formData.fields;
     
-    console.log('🔵 Données reçues:', fields);
+    const { fields, files } = await multipartParser.parse(event);
+    
+    console.log('🔵 Fields reçus:', fields);
+    console.log('🔵 Files reçus:', files);
 
+    
     if (!fields.teamName || !fields.captainName || !fields.tournamentId) {
       return {
         statusCode: 400,
@@ -36,10 +38,13 @@ exports.handler = async (event) => {
       };
     }
 
+    
     const sql = neon(process.env.DATABASE_URL);
+    
     
     const registrationCode = 'TT4-' + Math.random().toString(36).substring(2, 10).toUpperCase();
 
+    
     const result = await sql`
       INSERT INTO teams (
         team_name, team_tag, captain_name, captain_link,
@@ -82,24 +87,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
-
-function parseFormData(event) {
-  return new Promise((resolve, reject) => {
-    const form = new multiparty.Form();
-    
-    form.parse(event, (err, fields, files) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      
-      const parsedFields = {};
-      Object.keys(fields).forEach(key => {
-        parsedFields[key] = fields[key][0];
-      });
-      
-      resolve({ fields: parsedFields, files });
-    });
-  });
-}
