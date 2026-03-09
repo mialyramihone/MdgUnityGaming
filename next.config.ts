eba23a16b904b884a31600a6a842275a8395376c
mialyramihone <ramihonemialy@gmail.com>
2026-03-09 06:43:55 +0300

    config: Netlify + Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  
+  
+  output: 'standalone',
+  
+  
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
