1dee54a42f09d4d92782e316bb531ccceb5c81ae
mialyramihone <ramihonemialy@gmail.com>
2026-03-26 00:18:41 +0300

    Fix classement and resultat pages props

--- src/app/classement/page.tsx
+++ src/app/classement/page.tsx
@@ -2,27 +2,11 @@
 
 import Classement from '@/components/Classement';
 
-
-const tournois = [
-  {
-    id: 1,
-    titre: 'Tournoi Femina Esport',
-    date: '2026-03-08',
-    jeu: 'Free Fire'
-  },
-  {
-    id: 2,
-    titre: 'The Tournament saison 4',
-    date: '2026-03-23',
-    jeu: 'Free Fire'
-  }
-];
-
 export default function ClassementPage() {
   return (
     <div className="min-h-screen bg-white py-8">
       <div className="container mx-auto">
-        <Classement tournois={tournois} />
+        <Classement />
       </div>
     </div>
   );
